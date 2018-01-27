2dcc2db6f84594b8139661ab2ab665a203217160
Danakt Frost <danakt_frost@mail.ru>
2018-01-28 01:09:52 +0300

    Create store

---
+++ src/store/index.js
@@ -0,0 +1,66 @@
+import { createStore } from 'redux'
+import createMineField from '../utils/createMineField'
+
+/** @todo Move to some config */
+const DEFAULT_WIDTH = 9
+const DEFAULT_HEIGHT = 9
+const DEFAULT_MINES = 10
+
+/** Initial state */
+const initialState = {
+  width: DEFAULT_WIDTH,
+  height: DEFAULT_HEIGHT,
+  mines: DEFAULT_MINES,
+  // The mine field
+  minefield: createMineField(DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_MINES),
+  // The map of states of every item of minefield
+  // Empty 2d array on start
+  mapstate: Array(DEFAULT_WIDTH).map(column => new Uint8Array()),
+  // Flag of playing start
+  isPlaying: false,
+}
+
+/** The store of application */
+export default createStore((state = initialState, action) => {
+  switch (action.type) {
+    // Creating the minefield
+    case 'INIT': {
+      return {
+        ...state,
+        minefield: createMineField(state.width, state.height, state.mines),
+        mapstate: Array(state.width).fill(undefined).map(column => new Uint8Array(state.height)),
+        isPlaying: false,
+      }
+    }
+
+    // Starting the game
+    case 'START': {
+      return {
+        ...state,
+        isPlaying: true,
+      }
+    }
+
+    // Resize the minefield
+    case 'RESIZE': {
+      return {
+        ...state,
+        width: action.payload.width,
+        height: action.payload.heigth,
+      }
+    }
+
+    // Set mines number
+    case 'SET_MINES': {
+      return {
+        ...state,
+        mines: action.payload.mines,
+      }
+    }
+
+    // Fallback
+    default: {
+      return state
+    }
+  }
+})
